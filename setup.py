3e9988bbdb6f502e703bc2c834a88b4b6e6b1fe2
Joscha Wagner <joscha.wagner@me.com>
2024-02-08 22:01:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import pathlib
-
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
@@ -11,9 +10,16 @@ setup(
     description="A client library for accessing Rentman API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    package_dir={"": "rentman_api_client"},
     packages=find_packages(),
     python_requires=">=3.6, <4",
-    install_requires=["httpx >= 0.15.0, < 0.19.0", "attrs >= 20.1.0", "python-dateutil >= 2.8.0, < 3"],
-    package_data={"": ["CHANGELOG.md"], "rentman_api_client": ["py.typed"]},
+    install_requires=[
+        "httpx >= 0.15.0, < 0.19.0",
+        "attrs >= 20.1.0",
+        "python-dateutil >= 2.8.0, < 3"
+    ],
+    package_data={
+        "rentman_api_client": ["py.typed"],
+    },
+    classifiers=[
+    ],
 )
